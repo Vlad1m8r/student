d8ff412cddfd8d89c266fa39422e87625600cdef
Vladimir Goncharuk <VGoncharuk@lanit.ru>
2023-06-08 12:50:46 +0300

    2

--- react_lab/JS4.15.jsx
+++ react_lab/JS4.15.jsx
@@ -1,5 +1,33 @@
-class MyComponent extends Component {
-    render() {
-      return <div>Классовый!</div>;
-    }
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 0
+    };
   }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        seconds: prevState.seconds + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    const { seconds } = this.state;
+    return (
+      <div className="timer">
+        <span className="pulse">{seconds}</span> Таймер
+      </div>
+    );
+  }
+}
+
+export default Timer;
